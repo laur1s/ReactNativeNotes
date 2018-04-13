26696df33d9b7b982031f70b604d4deae06e13b6
Laurynas Tumosa <l.tumosa@gmail.com>
2018-04-13 14:30:22 +0100

    added notes

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Main from './App/Components/Main'
+
+export default class App extends React.Component {
+  render() {
+
+    return ( <Main />
+  
+    );
+  }
+}
+
